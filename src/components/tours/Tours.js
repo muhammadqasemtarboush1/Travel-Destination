38e6e0265bc338f95ec298037c393ded254382cd
muhammadqasemtarboush1 <muhammadtarboush1@gmail.com>
2022-05-15 14:38:39 +0300

    update: add new route & change rendering method to parent to child

--- src/components/tours/Tours.js
+++ src/components/tours/Tours.js
@@ -1,28 +1,12 @@
 import data from "../../data/db.json";
+import Tour from "./tour/Tour";
 import "./Tours.css";
 function Tours() {
   return (
     <div>
       <ul className="cards">
         {data.map((element, index) => {
-          return (
-            <li key={element.id}>
-              <div className="card">
-                <img
-                  src={element.image}
-                  className="card__image"
-                  alt={element.name}
-                />
-                <div className="card__header">
-                  <div className="card__header-text">
-                    <h3 className="card__title">{element.name}</h3>
-                    <span>{element.price}</span>
-                  </div>
-                </div>
-                <p className="card__description">{element.info}</p>
-              </div>
-            </li>
-          );
+          return <Tour data={element} />;
         })}
       </ul>
     </div>
